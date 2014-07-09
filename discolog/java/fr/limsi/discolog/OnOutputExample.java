14494d295419b314fe31e0bd845e5f5a9d9d2af0
Olydia <lydia.ouldouali@gmail.com>
2014-07-09 18:02:54 +0200

    mardi

--- discolog/java/fr/limsi/discolog/OnOutputExample.java
+++ discolog/java/fr/limsi/discolog/OnOutputExample.java
@@ -21,7 +21,7 @@ public static void main(String[] args) throws Exception {
 
 	//Struct go  = new Struct("pere", Term.createTerm("theodore"),Term.createTerm("geoffroy"));
 	
-	Theory theory = new Theory(new FileInputStream("C:/Users/ouldouali/Documents/GitHub/Discolog/discolog/prolog/regression-planner-orig/takeout.pl"));
+	Theory theory = new Theory(new FileInputStream("C:/Users/Lydia/Documents/GitHub/Discolog/discolog/prolog/regression-planner-orig/takeout.pl"));
 	//Theory theory = new Theory(":-consult('new.pl').");
 	try {
 	        engine.setTheory(theory);
@@ -29,19 +29,21 @@ public static void main(String[] args) throws Exception {
 	    } catch (InvalidTheoryException ex) {
 
 	    }
-	//Struct goal  = new Struct("test1",new Var("Plan"));
-	SolveInfo info = engine.solve("takeout(X,[1,2,3],L)");
+	Struct goal  = new Struct("takeout",new Var("X"),new Var("L"));
+	SolveInfo info = engine.solve("takeout(X,[1,2,3],L).");
 	System.out.println("trying to write the solution");
 	
 	 if (!info.isSuccess()) 
 		 	System.out.println("no." );
 	
 	 else if (!engine.hasOpenAlternatives()) {
-		 	System.out.println(info.getVarValue("Plan"));
+		 	System.out.println(info);
 	} else {// main case
-			System.out.println(info + " ?");
+			System.out.println(info);
 	}
-	 
+	 Term L = info.getVarValue("L");
+	 System.out.println(L.isList());
+	
 	Theory curTh = engine.getTheory(); // save current theory to file
 	//System.out.println(curTh.toString());
 	
