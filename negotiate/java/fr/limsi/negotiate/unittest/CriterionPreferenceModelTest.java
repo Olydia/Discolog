0e0284b250cd1483f7b4b1b509c6090740523b0f
Olydia <lydia.ouldouali@gmail.com>
2015-12-04 17:50:55 +0100

    testing

---
+++ negotiate/java/fr/limsi/negotiate/unittest/CriterionPreferenceModelTest.java
@@ -0,0 +1,19 @@
+package fr.limsi.negotiate.unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CriterionPreferenceModelTest {
+
+	@Test
+	public void testGenerateMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPreferred() {
+		fail("Not yet implemented");
+	}
+
+}
