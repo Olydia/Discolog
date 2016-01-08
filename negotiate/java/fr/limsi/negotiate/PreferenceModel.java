8bca12d86e4f48def3bf910e3ec44ee5679ee534
Olydia <lydia.ouldouali@gmail.com>
2016-01-08 15:37:32 +0100

    typo

--- negotiate/java/fr/limsi/negotiate/PreferenceModel.java
+++ negotiate/java/fr/limsi/negotiate/PreferenceModel.java
@@ -21,8 +21,8 @@ abstract public class PreferenceModel<C>{
 	 */
 	abstract public Boolean isPreferred (C more, C less);
 	 // A definir ici 
-	abstract public C getMostPreffered();
-	abstract public C getLeastPreffered();
+	abstract public C getMostPreferred();
+	abstract public C getLeastPreferred();
 	 public void updateLastPreference(Preference<C> preference) {
 		lastUpdate = preference;
 	}
