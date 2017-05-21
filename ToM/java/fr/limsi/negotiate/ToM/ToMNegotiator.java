47f7578bf0d7f1e2ae5f5726a826e997b530c7b8
Olydia <lydia.ouldouali@gmail.com>
2017-05-21 22:47:42 +0200

    update 4

--- ToM/java/fr/limsi/negotiate/ToM/ToMNegotiator.java
+++ ToM/java/fr/limsi/negotiate/ToM/ToMNegotiator.java
@@ -19,13 +19,13 @@ import fr.limsi.negotiate.lang.RejectState;
 
 public class ToMNegotiator extends NegotiatorAgent{
 	public ArrayList<Double> pow_hyp;
-	
+
 	public ToMNegotiator(String name, Negotiation<? extends Option> negotiation) {
 		super(name, negotiation);
 		setPow_hyp();
 
 	}
-		
+
 	@Override
 	public Utterance respond (Utterance utterance, Disco disco) {
 		Utterance u = respondTo(utterance, disco);
@@ -43,82 +43,85 @@ public class ToMNegotiator extends NegotiatorAgent{
 	}
 
 	//----------------------------------------- for ToM ----------------------------------------
-		public Utterance guessUtt(Negotiation<? extends Option> nego, double pow, Utterance utt, Disco disco){
-			NegotiatorAgent agent = new NegotiatorAgent("current", nego);
-			agent.setRelation(pow);
-			return agent.respondTo(utt, disco);
-		}
-		
-		public ArrayList<Double> guess (Negotiation<? extends Option> current, Disco disco, 
-								Utterance previousUtt, Utterance guessUtt ) {
-			ArrayList<Double> power = new ArrayList<Double>();
-			
-			for (double pow: this.pow_hyp){
-				current.setDominance(pow);
-				if(identicalUtterances(guessUtt, guessUtt(current, pow, previousUtt, disco)))
-					power.add(pow);
-			}
-			pow_hyp= power;
-			return power;
-		}
+	public Utterance guessUtt(Negotiation<? extends Option> nego, double pow, Utterance utt, Disco disco){
+		NegotiatorAgent agent = new NegotiatorAgent("current", nego);
+		agent.setRelation(pow);
+		return agent.respondTo(utt, disco);
+	}
 
-		public void setPow_hyp(){
-			
-			pow_hyp = 	new ArrayList<Double> ();
-			for(int i=1; i<10; i++){
-				pow_hyp.add(i/10.0);
-			}
+	public ArrayList<Double> guess (Negotiation<? extends Option> current, Disco disco, 
+			Utterance previousUtt, Utterance guessUtt ) {
+		ArrayList<Double> power = new ArrayList<Double>();
+
+		for (double pow: this.pow_hyp){
+			current.setDominance(pow);
+			if(identicalUtterances(guessUtt, guessUtt(current, pow, previousUtt, disco)))
+				power.add(pow);
 		}
+		pow_hyp= power;
+		return power;
+	}
+
+	public void setPow_hyp(){
+
+		pow_hyp = 	new ArrayList<Double> ();
+		for(int i=1; i<10; i++){
+			pow_hyp.add(i/10.0);
+		}
+	}
+
+	boolean identicalUtterances(Utterance ut, Utterance otherUt){
 		
-		boolean identicalUtterances(Utterance ut, Utterance otherUt){
-			if(ut.getType().equals(otherUt.getType())){
-				if (ut instanceof PreferenceUtterance){
-					Criterion c1 =  ((PreferenceUtterance) ut).getValue();
-					Criterion c2 =  ((PreferenceUtterance) otherUt).getValue();
-					if((c1 == null && c2!=null) || (c2 == null && c1!=null))
-						return false;
-					
+		if(ut.getType().equals(otherUt.getType())){
+			if (ut instanceof PreferenceUtterance){
+				Criterion c1 =  ((PreferenceUtterance) ut).getValue();
+				Criterion c2 =  ((PreferenceUtterance) otherUt).getValue();
+				if((c1 == null && c2!=null) || (c2 == null && c1!=null))
+					return false;
+				if(c1 == null && c2==null){
 					if(ut instanceof AskPreference){
 						Class<? extends Criterion> class1 = ((AskPreference) ut).getCriterion();
 						Class<? extends Criterion> class2 = ((AskPreference) otherUt).getCriterion();
-						
-						
+						return (class1.equals(class2));
 					}
-					else 
-						return (c1.equals(c2));
-
+					return false;	
 
 				}
-				else if (ut instanceof ProposalUtterance){
-					Proposal c1 =  ((ProposalUtterance) ut).getProposal();
-					Proposal c2 =  ((ProposalUtterance) otherUt).getProposal();
-					
-					if(ut instanceof AcceptPropose){
-						Proposal a1 = ((AcceptPropose) ut).getAccepted();
-						Proposal a2 = ((AcceptPropose) otherUt).getAccepted();
-						return (c1.equals(c2) && a1.equals(a2));
+			else 
+				return (c1.equals(c2));
 
-					}
-					else if(ut instanceof RejectPropose){
-						Proposal r1 = ((RejectPropose) ut).getReject();
-						Proposal r2 = ((RejectPropose) otherUt).getReject();
-						return (c1.equals(c2) && r1.equals(r2));	
-					}
-					else if (ut instanceof RejectState){
-						Criterion cr1 = ((RejectState) ut).getJustify();
-						Criterion cr2 = ((RejectState) otherUt).getJustify();
-						return (c1.equals(c2) && cr1.equals(cr2));	
 
-					}
-
-					
-					return (c1.equals(c2));
-
-				}
-				else if (ut instanceof Say)
-					return (((Say) ut).getText().equals(((Say) otherUt).getText()));
-			}
-			return false;
 		}
+		else if (ut instanceof ProposalUtterance){
+			Proposal c1 =  ((ProposalUtterance) ut).getProposal();
+			Proposal c2 =  ((ProposalUtterance) otherUt).getProposal();
+
+			if(ut instanceof AcceptPropose){
+				Proposal a1 = ((AcceptPropose) ut).getAccepted();
+				Proposal a2 = ((AcceptPropose) otherUt).getAccepted();
+				return (c1.equals(c2) && a1.equals(a2));
+
+			}
+			else if(ut instanceof RejectPropose){
+				Proposal r1 = ((RejectPropose) ut).getReject();
+				Proposal r2 = ((RejectPropose) otherUt).getReject();
+				return (c1.equals(c2) && r1.equals(r2));	
+			}
+			else if (ut instanceof RejectState){
+				Criterion cr1 = ((RejectState) ut).getJustify();
+				Criterion cr2 = ((RejectState) otherUt).getJustify();
+				return (c1.equals(c2) && cr1.equals(cr2));	
+
+			}
+
+
+			return (c1.equals(c2));
+
+		}
+		else if (ut instanceof Say)
+			return (((Say) ut).getText().equals(((Say) otherUt).getText()));
+	}
+	return false;
+}
 
 }
