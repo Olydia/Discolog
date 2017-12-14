25b169c8a779a252a395b91c8552d6a801fa2c8c
Olydia <ouldouali@boule>
2017-12-14 18:11:23 +0100

    imple

--- ToM/java/fr/limsi/negotiate/ToM/preferencesGeneration/ModelsTest.java
+++ ToM/java/fr/limsi/negotiate/ToM/preferencesGeneration/ModelsTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 /**
  * Created by PC on 11/11/2017.
