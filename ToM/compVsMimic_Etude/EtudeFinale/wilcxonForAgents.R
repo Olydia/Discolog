b2425283e3283490f74df6184192035d2521e019
Olydia <ouldouali@boule>
2018-04-20 18:30:39 +0200

    joint gain

--- ToM/compVsMimic_Etude/EtudeFinale/wilcxonForAgents.R
+++ ToM/compVsMimic_Etude/EtudeFinale/wilcxonForAgents.R
@@ -1,20 +1,19 @@
 #file path + name
 pValue <- function(xlsFile, sheet){
   
-  my_array<- read_xlsx(xlsFile,sheet = sheet)
-  noms <-names(my_array)
+  m<- read_xlsx(xlsFile,sheet = sheet)
   i = 1;
   
-  p1<- wilcox.test(H1, X__1, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
+  p1<- wilcox.test(m$H1, m$X__1, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
   print(p1)
   
-  p2<-  wilcox.test(H2, X__2, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
+  p2<-  wilcox.test(m$H2, m$X__2, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
   print(p2)
   
-  p3 <- wilcox.test(H3, X__3, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
+  p3 <- wilcox.test(m$H3, m$X__3, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
   print(p3)
   
-  p4 <-  wilcox.test(H4, X__4, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
+  p4 <-  wilcox.test(m$H4, m$X__4, mu=0, alt ="two.sided", paired =T, conf.int= T, conf.level = 0.95, exact=F, correct = F)
   print(p4)
 }
 
