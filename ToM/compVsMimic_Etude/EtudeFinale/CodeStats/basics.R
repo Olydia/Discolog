621e72d2348965a02d5a8abb6c4dc84a059d6c59
Olydia <ouldouali@boule>
2018-04-25 19:27:08 +0200

    modification

---
+++ ToM/compVsMimic_Etude/EtudeFinale/CodeStats/basics.R
@@ -0,0 +1,17 @@
+basic <- function(donne)
+{
+  noms <-names(donne)
+  print(noms)
+  i = 1
+  for(col in donne)
+  {
+   # print(noms[i])
+   # normality1 <- shapiro.test(col)
+   # print(normality1)
+    #normality2 <- ad.test(col)
+    # print(normality2)
+    cat(noms[i],";", mean(col), "; " , sd(col), "\n")
+    
+    i = i+1
+  }
+}
